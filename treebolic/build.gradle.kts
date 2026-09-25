b1a171056493c6afda250e45ab444b3e79ee7f66
treebolic <treebolic@gmail.com>
2026-09-25 15:06:55 +0200

    rethink signing

--- treebolic/build.gradle.kts
+++ treebolic/build.gradle.kts
@@ -28,7 +28,7 @@ plugins {
     alias(libs.plugins.androidApplication)
 }
 
-val keystorePropertiesFile: File = rootProject.file("keystore.properties")
+val keystorePropertiesFile: File = rootProject.file("keystore_treebolic.properties")
 val keystoreProperties: Properties = Properties()
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
@@ -55,7 +55,7 @@ android {
     }
 
     signingConfigs {
-        create("treebolic") {
+        create("release") {
             keyAlias = keystoreProperties.getProperty("keyAlias")
             keyPassword = keystoreProperties.getProperty("keyPassword")
             storeFile = file(keystoreProperties.getProperty("storeFile"))
@@ -67,7 +67,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.txt")
-            signingConfig = signingConfigs.getByName("treebolic")
+            signingConfig = signingConfigs.getByName("release")
             versionNameSuffix = "signed"
         }
     }
